42f39b692f5ce85bfe2cae4a2ae3c5960dc4a24c
hongtienhaolk2001 <hongtienhaolk2001@gmail.com>
2023-12-21 10:42:22 +0700

    231221

--- train/utils.py
+++ train/utils.py
@@ -9,8 +9,8 @@ def pred_to_label(classifier_outputs, regressor_outputs):
 
 
 def get_y(batch, classifier_outputs, regressor_outputs):
-    outputs_regressor = regressor_outputs.cpu().numpy()
-    outputs_regressor = outputs_regressor.argmax(axis=-1) + 1
+    regressor_outputs = regressor_outputs.cpu().numpy()
+    regressor_outputs = regressor_outputs.argmax(axis=-1) + 1
     y_true = batch['labels_regressor'].numpy()
     y_pred = np.round(pred_to_label(classifier_outputs.cpu().numpy(), regressor_outputs))
     return y_pred, y_true
